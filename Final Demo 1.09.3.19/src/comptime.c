a6be838f3f9668e20feb64ba224720805d25df47
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2021-03-21 06:41:06 +0000

    There's genuinely too many files here help

---
+++ Final Demo 1.09.3.19/src/comptime.c
@@ -0,0 +1,12 @@
+/*
+ * Compile date and time.
+ */
+
+const char *compdate = __DATE__;
+const char *comptime = __TIME__;
+/*
+ * Compile date and time.
+ */
+
+const char *compdate = __DATE__;
+const char *comptime = __TIME__;
